14464a3793cdb1a75f624dfca6b7cc4399b3f6c2
Ian <holmesian17@gmail.com>
2020-09-22 22:02:26 -0600

    Update scanSorter.py

--- scanSorter.py
+++ scanSorter.py
@@ -1,119 +1,111 @@
-
-'''
-import tkinter as tk
-import os
-from tkinter import filedialog
-from tkinter import *
-
-# WINDOW CREATION
-win = tk.Tk()
-geo = win.geometry
-geo("400x400+400+400")
-win['bg'] = 'orange'
-
-# get the list of files
-folder = filedialog.askdirectory(initialdir="/", title="Select a Folder")
-flist = os.listdir(folder)
-
-lbox = tk.Listbox(win)
-lbox.pack()
-
-# THE ITEMS INSERTED WITH A LOOP
-for item in flist:
-    lbox.insert(tk.END, item)
-
-
-def showcontent(event):
-    x = lbox.curselection()[0]
-    file = lbox.get(x)
-    with open(file, 'r', encoding='utf-8') as file:
-        file = file.read()
-    text.delete('1.0', tk.END)
-    text.insert(tk.END, file)
-
-
-text = tk.Text(win, bg='cyan')
-text.pack()
-# BINDING OF LISTBOX lbox
-lbox.bind("<<ListboxSelect>>", showcontent)
-# BUTTON
-
-win.mainloop()
-'''
-
-
 import tkinter as tk
 import os
 import sys
 from tkinter import filedialog
-from tkinter import *
 
 from PIL import Image
 from PIL import ImageTk
 
-# WINDOW CREATION
-win = tk.Tk()
-win.resizable(width=True, height=True)
-win['bg'] = 'orange'
+class SortingGui(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.grid(column=9)
+        self.create_widgets()
 
-lbox = tk.Listbox(win)
-lbox.grid(row=0, column=1)
+    def create_widgets(self):
+        self.fileBox = tk.Listbox(self, exportselection=False)
+        self.fileBox.bind("<<ListboxSelect>>", self.showContent)
+        self.fileBox.grid(row = 1, column=2)
 
-panel = tk.Canvas(win)
-panel.grid(row=0, column=0)
+        self.folderBox = tk.Listbox(self, exportselection=False)
+        self.folderBox.bind("<<ListboxSelect>>", self.currentFolder)
+        self.folderBox.grid(row=2, column=2)
 
-def getFolder():
-    lbox.delete(0, 'end')
-    folder = filedialog.askdirectory(initialdir="/", title="Select a Folder")
-    # get the list of files
-    flist = os.listdir(folder)
-    print(flist)
+        self.imageCanvas = tk.Canvas()
+        self.imageCanvas.grid(row=0, column=0)
 
-    # THE ITEMS INSERTED WITH A LOOP
-    fileTypes = (".tif", ".png")
-    for item in flist:
-        if item.endswith(fileTypes):
-            lbox.insert(tk.END, item)
-        else:
-            continue
-    def showcontent(x):
-        x = lbox.curselection()[0]
-        file = lbox.get(x)
-        file = os.path.join(folder, file)
+        self.moveCurrentButton = tk.Button(self, text='Move to Current Folder', command = self.moveToCurrent)
+        self.moveCurrentButton.grid(row=4, column=0)
+
+        self.newFolder = tk.Button(self, text='New Folder', command = self.createNewFolder)
+        self.newFolder.grid(row = 4, column = 1)
+
+        self.undoLastMove = tk.Button(self, text='Undo Move', command = self.undoMove)
+        self.undoLastMove.grid(row = 5, column = 1)
+
+        self.changeNewspaperTitle = tk.Button(self, text = 'Change Title', command = self.changeTitle)
+        self.changeNewspaperTitle.grid(row=4, column = 2)
+
+        self.changeNewspaperDate = tk.Button(self, text = 'Change Date', command = self.changeDate)
+
+        self.selectFolder = tk.Button(self, text="Select Reel", command=self.getFolder)
+        self.selectFolder.grid(row=4, column=3)
+
+        self.close = tk.Button(self, text="Exit", command=self.master.destroy)
+        self.close.grid(row=5, column=3)
+
+    def getFolder(self):
+        self.fileBox.delete(0, 'end')
+        self.folder = filedialog.askdirectory(initialdir="/", title="Select a Folder")
+        # get the list of files
+        flist = os.listdir(self.folder)
+
+        # THE ITEMS INSERTED WITH A LOOP
+        fileTypes = (".tif", ".png")
+        for item in flist:
+            isFolder = os.path.join(self.folder, item)
+            isdir = os.path.isdir(isFolder)
+            if item.endswith(fileTypes):
+                self.fileBox.insert(tk.END, item)
+            elif isdir == True:
+                self.folderBox.insert(tk.END, item)
+            else:
+                continue
+
+        print(flist)
+
+
+    def showContent(self, event):
+        widget = event.widget
+        selection = widget.curselection()
+        file = widget.get(selection[0])
+        folder = self.folder
+        file = os.path.join(self.folder, file)
         print(file)
         img = ImageTk.PhotoImage(Image.open(file))
-        panel.image = img
-        panel.create_image(20, 20, anchor=NW, image=img)
+        self.imageCanvas.image = img
+        self.imageCanvas.create_image(20, 20, image=img)
+
+    def currentFolder(self, event):
+        widget = event.widget
+        selection = widget.curselection()
+        file = widget.get(selection[0])
+        folder = self.folder
+        print(folder)
+
+    def moveToCurrent(self):
+        x = self.folderBox.curselection()
+        y = self.fileBox.curselection()
+        px = self.folderBox.get(x[0])
+        py = self.fileBox.get(y[0])
+        print(px)
+        print(py)
+        #probably need to use os.path connection
+
+    def createNewFolder(self):
+        print('')
+
+    def undoMove(self):
+        print('')
+
+    def changeTitle(self):
+        print('')
+
+    def changeDate(self):
+        print('')
 
 
-    lbox.bind("<<ListboxSelect>>", showcontent)
-
-button = tk.Button(win, text="Select Folder")
-button['command'] = getFolder
-button.grid(row=1)
-
-def closeSession():
-    win.destroy()
-
-close = tk.Button(win, text="Close")
-close['command'] = closeSession
-close.grid(column=2, row=1)
-
-
-
-'''
-def showcontent(event, audio=0):
-    x = lbox.curselection()[0]
-    file = lbox.get(x)
-    file = os.path.join(folder, file)
-    with open(file, 'r', encoding='utf-8') as file:
-        file = file.read()
-    text.delete('1.0', tk.END)
-    text.insert(tk.END, file)
-'''
-
-# BINDING OF LISTBOX lbox
-# lbox.bind("<<ListboxSelect>>", showcontent)
-# BUTTON
-
-win.mainloop()
+root = tk.Tk()
+app = SortingGui(master=root)
+app.mainloop()
